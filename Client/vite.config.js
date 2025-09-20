11748b2c617cb73fe54b7ffcc6719ee5fb6f7385
theprincevig <theprincevig@gmail.com>
2025-09-20 01:33:54 -0700

    do some changes in vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +17,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
   },
   plugins: [
-    react()
+    react(), tailwindcss()
   ],
   server: {
     proxy: {
